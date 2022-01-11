c811d7c6cb09ab912a91960f964d0214404b4efd
yoongyu-hwang <dotheji11@naver.com>
2022-01-11 14:53:23 +0900

    2022.01.11 14:53

--- app/src/main/java/com/example/albumcalc/MainActivity.kt
+++ app/src/main/java/com/example/albumcalc/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.albumcalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.albumcalc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val bu:Int = Integer.parseInt(binding.editBu.text.toString())
+            val meon:Int = Integer.parseInt(binding.editMeon.text.toString())
+
+            binding.FinalPrice.text = (bu * meon).toString()
+        }
+
     }
-    //asd
+
+
+
+
+
+
 }
